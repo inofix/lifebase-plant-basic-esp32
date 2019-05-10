c415808cb1f849d6590de55a719312a60a20ddb0
Michael Lustenberger <mic@inofix.ch>
2019-05-10 12:28:44 +0200

    New Sensors
    Add DHT and light exposure sensors.

--- main/lifebase_plant_basic_esp32_main.cpp
+++ main/lifebase_plant_basic_esp32_main.cpp
@@ -8,12 +8,24 @@
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
+#include "DHT_U.h"
+
+#define DHTPIN 13
+#define DHTTYPE DHT22
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+#define LIGHTPIN 34
 
 extern "C" {
    void app_main();
 }
 
 void app_main() {
+    initArduino();
+
+    dht.begin();
+
 
     printf("Hello user, this is your setup meter.\n");
 
@@ -29,6 +41,28 @@ void app_main() {
 
     printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
             (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
+    sensor_t sensor;
+    dht.temperature().getSensor(&sensor);
+    printf("Humidity/temperature sensor is %s.\n", sensor.name);
+    sensors_event_t event;
+    dht.temperature().getEvent(&event);
+    if (isnan(event.temperature)) {
+        printf("Error reading temperature!\n");
+    }
+    else {
+        printf("Current temperature is %f°C.\n", event.temperature);
+    }
+    dht.humidity().getSensor(&sensor);
+    dht.humidity().getEvent(&event);
+    if (isnan(event.relative_humidity)) {
+        printf("Error reading humidity!\n");
+    }
+    else {
+        printf("Current humidity is %f%%.\n", event.relative_humidity);
+    }
+
+    printf("Current light exposure is %d\n", analogRead(LIGHTPIN));
 
     for (int i = 10; i >= 0; i--) {
         printf("Restarting in %d seconds...\n", i);
