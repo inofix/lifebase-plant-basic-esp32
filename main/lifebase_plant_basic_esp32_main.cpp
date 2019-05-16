ba512250d5e04f6bd1fe4f46b35886f19f9254fb
Michael Lustenberger <mic@inofix.ch>
2019-05-17 00:03:05 +0200

    Log
    Use the log for now..

--- main/lifebase_plant_basic_esp32_main.cpp
+++ main/lifebase_plant_basic_esp32_main.cpp
@@ -13,7 +13,7 @@
 //#include "freertos/event_groups.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
-//#include "esp_log.h"
+#include "esp_log.h"
 //#include "nvs_flash.h"
 //#include "sdkconfig.h"
 
@@ -27,6 +27,9 @@
 
 // Sensor includes
 #include "DHT_U.h"
+
+// System constants
+#define LB_TAG "LifeBaseMeter"
 
 // Sensor constants
 #define DHTPIN 13
@@ -52,14 +55,14 @@ void get_board_info() {
     /* Print chip information */
     esp_chip_info_t chip_info;
     esp_chip_info(&chip_info);
-    printf("We are running on an ESP32 chip with %d CPU cores, WiFi%s%s, ",
+    ESP_LOGI(LB_TAG, "We are running on an ESP32 chip with %d CPU cores, WiFi%s%s, ",
             chip_info.cores,
             (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
             (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
 
-    printf("silicon revision %d, ", chip_info.revision);
+    ESP_LOGI(LB_TAG, "silicon revision %d, ", chip_info.revision);
 
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
+    ESP_LOGI(LB_TAG, "%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
             (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 }
 
@@ -73,30 +76,30 @@ void get_dht_info() {
 
     sensor_t sensor;
     dht.temperature().getSensor(&sensor);
-    printf("Humidity/temperature sensor is %s.\n", sensor.name);
+    ESP_LOGI(LB_TAG, "Humidity/temperature sensor is %s.\n", sensor.name);
     sensors_event_t event;
     dht.temperature().getEvent(&event);
     if (isnan(event.temperature)) {
-        printf("Error reading temperature!\n");
+        ESP_LOGI(LB_TAG, "Error reading temperature!\n");
     }
     else {
-        printf("Current temperature is %f°C.\n", event.temperature);
+        ESP_LOGI(LB_TAG, "Current temperature is %f°C.\n", event.temperature);
     }
     dht.humidity().getSensor(&sensor);
     dht.humidity().getEvent(&event);
     if (isnan(event.relative_humidity)) {
-        printf("Error reading humidity!\n");
+        ESP_LOGI(LB_TAG, "Error reading humidity!\n");
     }
     else {
-        printf("Current humidity is %f%%.\n", event.relative_humidity);
+        ESP_LOGI(LB_TAG, "Current humidity is %f%%.\n", event.relative_humidity);
     }
 }
 
 void get_sun_info() {
 
-    printf("Current light exposure is %d.\n", analogRead(LIGHTPIN));
-    printf("Current soil moisture reported from the 'mono' is %d.\n", analogRead(SOILMONOPIN));
-    printf("Current soil moisture reported from the 'dual' is %d, %d.\n", analogRead(SOILDUALAPIN), digitalRead(SOILDUALDPIN));
+    ESP_LOGI(LB_TAG, "Current light exposure is %d.\n", analogRead(LIGHTPIN));
+    ESP_LOGI(LB_TAG, "Current soil moisture reported from the 'mono' is %d.\n", analogRead(SOILMONOPIN));
+    ESP_LOGI(LB_TAG, "Current soil moisture reported from the 'dual' is %d, %d.\n", analogRead(SOILDUALAPIN), digitalRead(SOILDUALDPIN));
 }
 
 //void get_shade_info() {
@@ -106,15 +109,16 @@ void get_sun_info() {
 
 void get_cachepot_info() {
 
-    printf("Current water level reported is %d.\n", analogRead(WATERPIN));
+    ESP_LOGI(LB_TAG, "Current water level reported is %d.\n", analogRead(WATERPIN));
 }
 
 void app_main() {
 
     init_sensors();
 
+    get_board_info();
+
     while (true) {
-        get_board_info();
         get_dht_info();
 
         get_sun_info();
